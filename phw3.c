aae7d91dbfd7d8fe3c149dfadf8b12eadfcf4008
zemehuishi <1954226364@qq.com>
2023-12-13 11:35:10 +0800

    homework

--- phw3.c
+++ phw3.c
@@ -1,7 +1,18 @@
-void Mystrcpy(char *src, char *des, int m)
-{
-    *src + m;
-    for(;m < 1024 - m; m++){
-        *des++ = *src++;
-    }
+void Mystrcpy(char *src, char *des, int m) {
+  if (src == NULL || des == NULL || m < 1) {
+    return;
+  }
+  int len = strlen(src);
+
+  if (m > len) {
+    return;
+  }
+  int i = m - 1;
+  int j = 0; 
+  while (src[i] != '\0') {
+    des[j] = src[i];
+    i++; 
+    j++;
+  }
+  des[j] = '\0'; 
 }
